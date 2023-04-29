f0c7bd1927dabbbe90643e90586cc4fe5b7bf899
imaxisXD <sunny735084@gmail.com>
2023-04-29 14:38:33 +0530

    Added: added logo and html

---
+++ src/Component/NavLink.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NavLink() {
+    return (
+        <ul>
+            <li>
+                <Link href="/">Trade</Link>
+            </li>
+            <li>
+                <Link href="/">Earn</Link>
+            </li>
+            <li>
+                <Link href="/">Support</Link>
+            </li>
+            <li>
+                <Link href="/">About</Link>
+            </li>
+        </ul>
+    )
+}
